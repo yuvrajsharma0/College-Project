d610d3494244fb8c2054bff37f43b2a366b1b1c8
berserkeryuvraj <berserkeryuvraj@gmail.com>
2022-03-27 23:02:51 +0530

    u

---
+++ data_indexer.py
@@ -0,0 +1,43 @@
+import os
+import pickle
+import tqdm as tqdm
+from feature_extractor import FeatureExtractor
+import sys
+
+extensions = ['.jpg', '.JPG', '.jpeg', '.JPEG', '.png', '.PNG']
+
+
+def get_file_list(root_dir):
+    file_list = []
+    counter = 1
+    for root, directories, filenames in os.walk(root_dir):
+        for filename in filenames:
+            if any(ext in filename for ext in extensions):
+                file_list.append(os.path.join(root, filename))
+                counter += 1
+    return file_list
+
+
+# path to the datasets
+path = 'caltech101'
+
+if len(sys.argv) == 2:
+    path = sys.argv[1]
+
+root_dir = path
+filenames = sorted(get_file_list(root_dir))
+print("Total Files: " + str(len(filenames)))
+
+# Extracting Images features
+feature_list = []
+for i in tqdm.tqdm(range(len(filenames))):
+    feature_list.append(FeatureExtractor.extract(filenames[i]))
+
+
+# Saving data in pickle file
+pickle.dump(feature_list, open('data/features.pickle', 'wb'))
+print("Pickle Dump success on location: data/features.pickle")
+
+# Saving Filenames in pickle File
+pickle.dump(filenames, open('data/filenames.pickle','wb'))
+print("Pickle Dump success on location: data/filenames.pickle")
---
+++ feature_extractor.py
@@ -0,0 +1,38 @@
+from keras.preprocessing import image
+from keras.applications.resnet import ResNet50, preprocess_input
+import numpy as np
+
+
+class FeatureExtractor:
+    model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3), pooling='max')
+
+    @staticmethod
+    def extract(img_path):
+        # Loading Image using image path
+        img = image.load_img(img_path)
+        
+        # ResNet50 must take a 224x224 img as an input
+        img = img.resize((224, 224))
+        
+        # Make sure img is color
+        img = img.convert('RGB')
+
+        # To np.array. Height x Width x Channel. dtype = float32
+        img_array = image.img_to_array(img)
+
+        # (H, W, C)->(1, H, W, C), where the first elem is the number of img
+        expanded_img_array = np.expand_dims(img_array, axis=0)
+
+        # Subtracting avg values for each pixel
+        preprocessed_img = preprocess_input(expanded_img_array)
+
+        # (1, 4096) -> (4096, )
+        features = FeatureExtractor.model.predict(preprocessed_img)  
+
+        flattened_features = features.flatten()
+
+        # Normalize
+        normalized_features = flattened_features / np.linalg.norm(flattened_features)
+
+        # Return data
+        return normalized_features
